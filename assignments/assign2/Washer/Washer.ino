24105f825ddeefc9bb9e8ac999a2a576b3558ce3
Batman <Batman@Batman-PC.dhcp.wustl.edu>
2016-09-21 10:35:31 -0500

    Liam Joyce and Michael Billington. Did not end up finishing stoplight during studio.

--- assignments/assign2/Washer/Washer.ino
+++ assignments/assign2/Washer/Washer.ino
@@ -1,16 +1,105 @@
 //Fill in the values below with the pins that you chose to use
-const int POT_PIN = ;
-const int BUTTON_PIN = ;
+/* PIN PLACES
+ * 3 - button input, LOCK DOOR
+ * 5 - yellow, DRY
+ * 7 - red, HOT WASH
+ * 9 - green, COLD WASH
+ * A0 - potentiaomterer, CYCLE
 
-const int HOT_PIN = ;
-const int COLD_PIN = ;
-const int DRY_PIN = ;
-const int LOCK_PIN = ;
+*/
+const int POT_PIN = A0; 
+const int BUTTON_PIN = 3;
+
+const int HOT_PIN = 7;
+const int COLD_PIN = 9;
+const int DRY_PIN = 5;
+const int LOCK_PIN = 13;
+
+enum State{initialState,coldWash,hotWash,medWash,shortDry,longDry};
+State currentState= initialState;
 
 void setup() {
-
+  Serial.begin(9600);
+  //potentiometer
+  pinMode(A0, INPUT_PULLUP);
+  //button
+  pinMode(3,INPUT_PULLUP);
+  
 }
-
+int econ = 740;
+int sdeluxe = 330;
 void loop() {
-
+  switch (currentState) {
+     case initialState:
+     digitalWrite(LOCK_PIN, LOW);
+     if(digitalRead(BUTTON_PIN) == 1){
+        break;
+        }
+     else {
+        if(analogRead(A0) > econ){
+         currentState = coldWash;
+       }
+        else {
+         currentState = hotWash;
+       }
+     }
+  break;
+  case coldWash:
+     digitalWrite(LOCK_PIN, HIGH);
+     digitalWrite(COLD_PIN, HIGH);
+     delay(5000);
+     digitalWrite(COLD_PIN, LOW);
+        if(analogRead(A0) > econ){
+           currentState = shortDry;
+     }
+     else {
+    currentState = longDry;
+   }
+  break;
+ case shortDry:
+  digitalWrite(LOCK_PIN, HIGH);
+  digitalWrite(DRY_PIN, HIGH);
+  delay(2000);
+  digitalWrite(DRY_PIN, LOW);
+  currentState = initialState;
+ break;
+//TAKE NOTE MOFOS
+   break;
+   case hotWash:
+     digitalWrite(LOCK_PIN, HIGH);
+     digitalWrite(HOT_PIN, HIGH);
+     delay(7000);
+     digitalWrite(HOT_PIN, LOW);
+      if(analogRead(A0)< sdeluxe ){
+        currentState = medWash;
+      }
+      else if (analogRead(A0)>econ ){
+        currentState = shortDry;
+      }
+      else {
+        currentState = longDry;
+      }
+      break;
+  case medWash:
+    digitalWrite(LOCK_PIN, HIGH);
+    digitalWrite(COLD_PIN, HIGH);
+    digitalWrite(HOT_PIN, HIGH);
+    delay(7000);
+    digitalWrite(COLD_PIN,LOW);
+    digitalWrite(HOT_PIN,LOW);
+      if(analogRead(A0)<sdeluxe){
+        currentState = longDry;
+      }
+      else{
+  currentState = shortDry;
+      }
+   break;
+  case longDry:
+    digitalWrite(LOCK_PIN, HIGH);
+    digitalWrite(DRY_PIN, HIGH);
+    delay(7000);
+    digitalWrite(DRY_PIN, LOW);
+    currentState = initialState;
 }
+}
+ 
--- studios/studio3/stoplight/stoplight.ino
+++ studios/studio3/stoplight/stoplight.ino
@@ -4,26 +4,98 @@
  *  
  *  Fill this out so we know whose studio this is.
  *  
- *  Name:
- *  WUSTL Key:
+ *  Name: liam joyce 
+ *  WUSTL Key: 436678 
  *  
- *  Name:
- *  WUSTL Key:
- *  
- *  Name:
- *  WUSTL Key:
- *  
- *  Name:
+ *  Name: Michael Billington (partner)
  *  WUSTL Key:
  */
+ // EW RYG 5,4,3
+ // NS RYG 10,9,8
+ // WALK 6
+ // DONT WALK 7
+enum State{NSG, NSY, EWG, EWY, PED};
+State currentState = EWG;
 
+const int NSG_PIN = 10;
+const int NSY_PIN = 9;
+const int NSR_PIN = 8;
+const int EWR_PIN = 5;
+const int EWY_PIN = 4;
+const int EWG_PIN = 3;
+const int WALK = 6;
+const int NOWALK = 7;
+unsigned long Tnsg = 1087;
+unsigned long Tnsy = 457;
+unsigned long Tewg = 1087;
+unsigned long Tewy = 373;
+unsigned long Tped = 3041;
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
 
 }
-
+//currently stuck on first state
 void loop() {
-  // put your main code here, to run repeatedly:
+  switch(currentState) {
+    case EWG:
+      while (millis() > Tewg){
+        //Tewg += Tewg;
+       digitalWrite(EWG_PIN, HIGH);
+       digitalWrite(NSR_PIN, HIGH);
+       digitalWrite(NOWALK, HIGH);
+       }
+    digitalWrite(EWG_PIN, LOW);
+    digitalWrite(NSR_PIN, LOW);
+    currentState = EWY;
+    break;
+    
+    case EWY:
+      while (millis() > Tewy){
+        //Tewg += Tewg;
+       digitalWrite(EWY_PIN, HIGH);
+       digitalWrite(NSR_PIN, HIGH);
+       digitalWrite(NOWALK, HIGH);
+       }
+    digitalWrite(EWY_PIN, LOW);
+    digitalWrite(NSR_PIN, LOW);
+    currentState = PED;
+    break;
+    
+    case PED:
+      while (millis() > Tped){
+       digitalWrite(WALK, HIGH);
+       digitalWrite(EWR_PIN, HIGH);
+       digitalWrite(NSR_PIN, HIGH);
+       }
+       digitalWrite(WALK, LOW);
+       digitalWrite(EWR_PIN, LOW);
+       digitalWrite(NSR_PIN, LOW);
+    currentState = NSG;   
+    break;
+         
+    case NSG:
+      while (millis() > Tnsg){
+        digitalWrite(NSG_PIN, HIGH);
+        digitalWrite(EWR_PIN, HIGH);
+        digitalWrite(NOWALK, HIGH);
+      }
+    digitalWrite(NSG_PIN, LOW);
+    digitalWrite(EWR_PIN, LOW);
+    currentState = NSY;
+    break;
 
+    case NSY:
+      while (millis() > Tnsy){
+        digitalWrite(NSY_PIN, HIGH);
+        digitalWrite(EWR_PIN, HIGH);
+        digitalWrite(NOWALK, HIGH);
+      }
+      digitalWrite(NSY_PIN, LOW);
+      digitalWrite(EWR_PIN, LOW);
+      currentState = EWG;
+      break;
+  }
 }
+
